63844c8c4f68a002870f5e4397042eb65a42cc87
Erik Demko <erik.demko162@gmail.com>
2024-06-11 13:47:15 +0200

    importing data - tags

--- db/db-data/tags.sql
+++ db/db-data/tags.sql
@@ -1,104 +1,71 @@
-3dtlac
-ada
-airsoft
-alex
-alibaba
-alkohol
-andreaB
-antik
-auto
-bajk
-bazos
-bicykel
-biznis
-bolt
-boltfood
-byt
-cyklistika
-darcek
-darceky
-dogtag
-domacnost
-doprava
-drogeria
-elektronika
-filament
-filamenty
-ford
-foto
-futbalek
-gaming
-gitara
-hobby
-hry
-hudba
-investicia
-jasle
-jedlo
-kat
-kaufland
-kia
-kodi
-kupelna
-latino
-lidl
-lieky
-lyzovanie
-masaznePistole
-maso
-mcdonnald
-mk52
-mobil
-mobilne sluzby
-myska
-nabytok/byt/spotrebice
-najom
-nezname
-oblecenie
-obuv
-paddleboard
-paddleboardy
-pajkovanie
-paracord
-parkovanie
-parkovne
-party
-pausal
-pc
-phm
-pingpong
-pizza
-plechovky
-pochutiny
-podlozka
-poker
-pokuta
-posilka
-potraviny
-psikovia
-pzp
-reklama
-ria
-snowboard
-snp62
-spolocenskaZabava
-sport
-spotrebice
-steam
-susicka
-svadba
-tanecna
-tatrabanka
-taxi
-telekom
-topovanie
-toyota
-vylet
-vymedzovacie
-vyplata
-youtube
-zabava
-zahrada
-zbrane
-zdravie
-zubar
+('5d22ddf9-5e7a-4498-8033-9fffb72d4808','3dtlac','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('60b94b93-f774-4a8d-b99b-9935fdfd894a','airsoft','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('c2c6bab1-8284-461f-9a22-371983c9020f','alibaba','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('3daa8687-8e1c-4950-a617-c70ab43e2883','alkohol','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('59a96936-1147-48a6-a542-444001a9b0ce','andreaB','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('5aa71e67-7341-4fb6-94a2-8e93b8c78f57','auto','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('ce69a052-a8a3-445c-bb8e-62a3e29fc357','bazos','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('8104fa62-6845-4084-9948-3d62bac49813','biznis','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('097bf315-ef0a-4e02-892b-fd9099c9bcea','byt','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('b7048853-b3e5-452e-8fd3-94dbcdef23bd','cyklistika','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('34e2bb35-512b-483c-96ef-b2f5c645a3af','darceky','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('4eb78f5a-36b0-4d26-811f-52c6a7918351','dogtag','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('1f237362-b084-4d05-be49-ba1b0dcd6e9b','domacnost','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('19fb9506-260b-4b66-b386-06f2191c4d91','doprava','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('34ff7017-a050-40cd-ae59-f35bbcacb437','drogeria','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('3444ad93-e15f-48a0-9b05-8c60f3f9027d','elektronika','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('662bffd5-796a-441d-85f8-eac9bb3e6288','filamenty','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('3abddae4-dd5f-4b9c-94b7-464fd4eae78e','ford','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('524dd555-5a89-4b56-a6eb-d9c7df98c2b1','fotografovanie','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('14d4644e-9558-4627-8269-64ca1c5982bc','gaming','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('e2b54fa7-813e-4a1c-b94a-30892a736f5f','gitara','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('a1fcb768-d3b6-4d98-a8cc-85bcb3737de9','hobby','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('5e783073-1837-4adc-b4ec-498f0d99f725','investicia','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('f6777542-fd10-4331-92c4-088f53063980','jasle','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('032ef851-ac6b-4923-907c-3743ad8db6a1','jedlo','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('3f21f0e8-4632-4275-a67c-fa60ad62cff1','kat','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('b69ba1c4-36ac-4048-b92b-bc1a0e4b9ccc','kaufland','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('f5bedca5-6860-4d3e-ad4d-912814db9939','kia','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('674b4386-abd4-48c5-8e84-1a368995f122','kodi','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('8bd6d618-7d15-440d-8c53-7c60dc40ccb1','masaznePistole','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('b76046f4-bc90-4f02-aa28-649dd2f6bc08','mobil','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('4376784a-ac38-423d-b95b-2d8cccb29880','nabytok/byt/spotrebice','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('6be12cdd-cd83-4979-8977-4e787f0660d9','najom','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('e998da54-bcb3-441a-8afb-2d8a1cd7e44b','nezname','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('601919bf-90d6-478a-8f19-1e050c1952ed','oblecenie','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('c6f124c3-2e90-464b-8ad8-b93f6bc5da83','obuv','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('bdb11e00-9daa-48af-bf88-edfcbc416bd3','paddleboardy','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('378200b1-3925-4078-adde-b2c8d929cbef','parkovne','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('70552268-a07b-451a-b7f6-f9daee0515ba','party','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('884987ec-7694-4b4b-bae4-eba578028946','pausal','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('a84825a5-7096-4b35-a0e9-a7252d746205','pc','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('e6f8f18b-5611-4a10-976f-8cebf8894e5b','phm','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('3727125f-30e5-4ecb-ab92-53863c54b71f','plechovky','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('08375440-e347-4390-816e-1961597858eb','pochutiny','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('ff50aa5e-4282-40e5-aa76-afab3f6399a8','podlozka','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('7326f9c8-1026-48e1-9871-52c9ebdada64','poker','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('aedb3369-988e-4ac3-b37e-bfcdb00ea0ab','pokuta','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('30bccc21-3aac-4dc7-bb9b-bc004a6c16dc','cvicenie','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('71e072da-75ba-43bb-9953-ccbd64fc85e1','potraviny','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('9d7afdec-e06e-4dab-b612-547592c5ea4a','psikovia','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('7712058a-656c-4cb5-bf26-1d78cc5fdde5','pzp','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('e818f1a1-7d9e-45e2-a1b9-763a1070d89b','ria','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('e2152ee7-0fc9-42f7-bab5-edb7c9ab8c8e','snowboard','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('7572bb7d-43ef-4e29-a3d5-66accc21e5cd','snp62','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('47118b6e-d027-460a-ae6a-53eef3077c01','spolocenskaZabava','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('ea3e6d4a-cc4c-469b-887c-7d74f2c8f12e','sport','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('92789074-406b-41be-b2f1-f0fd4d252312','spotrebice','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('2481def1-5534-4ca4-8052-5b6daaa896bf','svadba','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('cb701c4c-bf36-452e-8cac-d4025932092f','tanecna','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('0e731c6e-f7ab-4675-b191-5e594b73891d','tatrabanka','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('461902f8-8b08-4251-bae1-98e265b37de7','taxi','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('d6c92aa9-86c6-40d3-94b4-f8fea9cc8af4','toyota','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('49e574f2-f507-4a60-85b9-182b1767dc07','vylet','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('be218009-f8ce-440b-90f6-c902fe9e292a','vymedzovacie','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('8528d160-98ef-4e38-882a-f584910e9eb3','vyplata','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('fc76cc2e-435d-4154-bbb4-9920797c1ab3','youtube','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('ba7ce7f0-63e2-4888-ade6-2c94554ad2a5','zabava','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('320aca0c-cd73-4348-a731-499a057183df','zahrada','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('d724bb15-685d-4b80-9ee3-4bee585fa7ec','zbrane','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('4d7ef362-f68a-46c9-b760-94645dc0926b','zdravie','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL),
+('03eb8b78-c245-4f3a-ac69-8e2fd8f57050','zubar','e66562a4-dcd9-498e-8977-414349a0df65', NOW(), NOW(), NULL);
