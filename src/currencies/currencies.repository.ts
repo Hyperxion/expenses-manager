a49d006d6c6cf3de7a38ec3441e491c3ea1811f8
Erik Demko <erik.demko162@gmail.com>
2025-03-11 21:18:55 +0100

    unused import

--- src/currencies/currencies.repository.ts
+++ src/currencies/currencies.repository.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { DataSource } from 'typeorm';
 import { BaseRepository } from '../BaseRepository';
 import { Currency } from './entities/currency.entity';
-import { processError } from '../constants';
 
 @Injectable()
 export class CurrenciesRepository extends BaseRepository<Currency> {
