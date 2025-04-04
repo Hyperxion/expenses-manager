70a5943142055ee6ceb974c7a1ecb60c546aac04
edemko <dddpsk@gmail.com>
2025-04-04 22:09:34 +0200

    xv

--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -43,6 +43,7 @@ export class TransactionsController {
     @GetUserId() userId: string,
   ): Promise<any> {
     try {
+      // GetUserId() decorator not working
       const uploadedFile = file?.buffer.toString();
 
       if (!uploadedFile) throw new Error('404');
